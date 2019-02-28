0c56219bc23db1cda44125ad3a9a6cd0f8b54418
BrendanVotano <brendan@blue-volcano.com>
2019-03-01 09:20:44 +1100

    Initial Commit

---
+++ Assets/Scripts/MyScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyScript : MonoBehaviour
+{
+    public float playerSpeed;
+    public float speedBurst = 2;
+    public int ammo = 400;
+    //public int health = 100;
+    public float playerAccuracyForThisMatch = 15.5f;
+    public string player = "Brendan";
+    public Vector3 starting;
+
+    void Start()
+    {
+        if(ammo <= 10)
+        {
+            Debug.Log("Ammo is low");
+        }
+        Debug.Log("My Ammo Is: " + ammo);
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        playerSpeed = playerSpeed + speedBurst;
+
+        //Moves out player in the forward direction every frame by player speed
+        transform.Translate(Vector3.forward * playerSpeed);
+
+        //makes the player jump by 2 units
+    }
+}
